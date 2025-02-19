6528a83bfc3360cdaf926c9ddf40df82d5cc1764
Damian Wąsik <damianwasik98@gmail.com>
2025-02-19 09:55:10 +0100

    change fzf setup

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -1,13 +1,3 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
-fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+source <(fzf --zsh)
 
-# Key bindings
-# ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
